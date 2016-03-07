dbbfbbbb03aa6afe8442f0db644f085a91787e86
Nicky Keyse <Nicky Keyse>
2016-03-07 21:41:34 +0000

    Altered CoffeeShop model to use MongoDs. Added bootscript to create data in the database.

---
+++ server/boot/create-sample-models.js
@@ -0,0 +1,17 @@
+// Scripts placed in server/boot will run at server start.
+// This script will drop and recreate test data in the database.
+module.exports = function(app) {
+  app.dataSources.MongDs.automigrate('CoffeeShop', function(err) {
+    if (err) throw err;
+ 
+    app.models.CoffeeShop.create([
+      {name: 'Bel Cafe', city: 'Vancouver'},
+      {name: 'Three Bees Coffee House', city: 'San Mateo'},
+      {name: 'Caffe Artigiano', city: 'Vancouver'},
+    ], function(err, coffeeShops) {
+      if (err) throw err;
+ 
+      console.log('Models created: \n', coffeeShops);
+    });
+  });
+};
