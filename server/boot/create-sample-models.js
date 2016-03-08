f05fb9052919b68edb048e58c49f018e2d4491c7
Nicky Keyse <Nicky Keyse>
2016-03-08 18:22:37 +0000

    Edited to use mongDs.

--- server/boot/create-sample-models.js
+++ server/boot/create-sample-models.js
@@ -1,16 +1,16 @@
 // Scripts placed in server/boot will run at server start.
 // This script will drop and recreate test data in the database.
 module.exports = function(app) {
-  app.dataSources.MongoDs.automigrate('CoffeeShop', function(err) {
+  app.dataSources.mongoDs.automigrate('CoffeeShop', function(err) {
     if (err) throw err;
- 
+
     app.models.CoffeeShop.create([
       {name: 'Bel Cafe', city: 'Vancouver'},
       {name: 'Three Bees Coffee House', city: 'San Mateo'},
       {name: 'Caffe Artigiano', city: 'Vancouver'},
     ], function(err, coffeeShops) {
       if (err) throw err;
- 
+
       console.log('Models created: \n', coffeeShops);
     });
   });
