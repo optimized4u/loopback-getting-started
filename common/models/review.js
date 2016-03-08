34aa4eb1c2110220041502233c731e35502a44b8
Nicky Keyse <Nicky Keyse>
2016-03-08 19:49:11 +0000

    Defined Remote Hook to set date and publisherID.

--- common/models/review.js
+++ common/models/review.js
@@ -1,3 +1,12 @@
 module.exports = function(Review) {
-
+  // Defines a Remote Hook on Review Model.
+  // Called before a new instance of the Review model is created.
+  Review.beforeRemote('create', function(context, user, next) {
+    var req = context.req;
+    // Set the date of the review instance to the current date.
+    req.body.date = Date.now();
+    // Insert the publisherId using the access token attached to the request.
+    req.body.publisherId = req.accessToken.userId;
+    next();
+  });
 };
