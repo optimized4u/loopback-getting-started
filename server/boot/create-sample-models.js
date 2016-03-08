0228e3975eddfaa626888a35e78ba28a39db4949
Nicky Keyse <Nicky Keyse>
2016-03-08 18:41:21 +0000

    Changed CoffeeShop sample model to use mongoDs.

--- server/boot/create-sample-models.js
+++ server/boot/create-sample-models.js
@@ -29,7 +29,7 @@ module.exports = function(app) {
   }
   //create coffee shops
   function createCoffeeShops(cb) {
-    mysqlDs.automigrate('CoffeeShop', function(err) {
+    mongoDs.automigrate('CoffeeShop', function(err) {
       if (err) return cb(err);
       var CoffeeShop = app.models.CoffeeShop;
       CoffeeShop.create([
